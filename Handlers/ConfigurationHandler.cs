e9e6e23ee5656c88b766f195db9360e3f071e6a0
Aaron <aaron@umbhost.net>
2023-07-05 20:35:03 +0100

    Added handlers

---
+++ Handlers/ConfigurationHandler.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Logging;
+using UmbCheckout.Core.Interfaces;
+using UmbCheckout.Shared.Models;
+using UmbCheckout.Shared.Notifications.Configuration;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Strings;
+using uSync.BackOffice;
+using uSync.BackOffice.Configuration;
+using uSync.BackOffice.Services;
+using uSync.BackOffice.SyncHandlers;
+using uSync.Core;
+
+namespace UmbCheckout.uSync.Handlers
+{
+    [SyncHandler("umbCheckoutHander", Consts.Configuration.HandlerName, Consts.Configuration.SerializerFolder, 1,
+        Icon = "icon-settings usync-addon-icon", EntityType = Consts.Configuration.EntityType)]
+    public class ConfigurationHandler : SyncHandlerRoot<UmbCheckoutConfiguration, UmbCheckoutConfiguration>, ISyncHandler,
+        INotificationHandler<OnConfigurationSavedNotification>
+    {
+        public override string Group => Consts.Group;
+
+        private readonly IConfigurationService _configurationService;
+        public ConfigurationHandler(ILogger<SyncHandlerRoot<UmbCheckoutConfiguration, UmbCheckoutConfiguration>> logger, AppCaches appCaches, IShortStringHelper shortStringHelper, SyncFileService syncFileService, uSyncEventService mutexService, uSyncConfigService uSyncConfig, ISyncItemFactory itemFactory, IConfigurationService configurationService) : base(logger, appCaches, shortStringHelper, syncFileService, mutexService, uSyncConfig, itemFactory)
+        {
+            _configurationService = configurationService;
+
+            itemContainerType = UmbracoObjectTypes.Unknown;
+        }
+
+        public override IEnumerable<uSyncAction> ExportAll(UmbCheckoutConfiguration parent, string folder, HandlerSettings config,
+            SyncUpdateCallback callback)
+        {
+            var item = _configurationService.GetConfiguration().Result;
+
+            var actions = new List<uSyncAction>();
+            if (item != null)
+            {
+                actions.AddRange(Export(item, Path.Combine(rootFolder, DefaultFolder), DefaultConfig));
+            }
+
+            return actions;
+        }
+
+        public void Handle(OnConfigurationSavedNotification notification)
+        {
+            if (!ShouldProcess()) return;
+
+            try
+            {
+                var attempts = Export(notification.Configuration, Path.Combine(rootFolder, DefaultFolder), DefaultConfig);
+                foreach (var attempt in attempts.Where(x => x.Success))
+                {
+                    CleanUp(notification.Configuration, attempt.FileName, Path.Combine(rootFolder, DefaultFolder));
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex, "uSync Save error");
+            }
+        }
+
+        protected override IEnumerable<uSyncAction> DeleteMissingItems(UmbCheckoutConfiguration parent, IEnumerable<Guid> keysToKeep, bool reportOnly)
+            => Enumerable.Empty<uSyncAction>();
+
+        protected override IEnumerable<UmbCheckoutConfiguration> GetChildItems(UmbCheckoutConfiguration parent)
+            => Enumerable.Empty<UmbCheckoutConfiguration>();
+
+        protected override IEnumerable<UmbCheckoutConfiguration> GetFolders(UmbCheckoutConfiguration parent)
+            => Enumerable.Empty<UmbCheckoutConfiguration>();
+
+        protected override UmbCheckoutConfiguration GetFromService(UmbCheckoutConfiguration item)
+            => _configurationService.GetConfiguration().Result ?? new UmbCheckoutConfiguration();
+
+        protected override string GetItemName(UmbCheckoutConfiguration item)
+            => item.Id.ToString();
+
+        protected override string GetItemFileName(UmbCheckoutConfiguration item)
+            => Consts.Configuration.FileName;
+
+        private bool ShouldProcess()
+        {
+            if (_mutexService.IsPaused) return false;
+            if (!DefaultConfig.Enabled) return false;
+            return true;
+        }
+    }
+}
---
+++ Serializers/ConfigurationSerializer.cs
@@ -0,0 +1,86 @@
+﻿using System.Xml.Linq;
+using Microsoft.Extensions.Logging;
+using UmbCheckout.Core.Interfaces;
+using UmbCheckout.Shared.Models;
+using uSync.Core;
+using uSync.Core.Models;
+using uSync.Core.Serialization;
+
+namespace UmbCheckout.uSync.Serializers
+{
+    [SyncSerializer("10A73824-7104-4A37-ADDB-118829041283", Consts.Configuration.SerializerName, Consts.Configuration.ItemType)]
+    public class ConfigurationSerializer : SyncSerializerRoot<UmbCheckoutConfiguration>, ISyncSerializer<UmbCheckoutConfiguration>
+    {
+        private readonly IConfigurationService _configurationService;
+        public ConfigurationSerializer(ILogger<SyncSerializerRoot<UmbCheckoutConfiguration>> logger, IConfigurationService configurationService) : base(logger)
+        {
+            _configurationService = configurationService;
+        }
+
+        protected override SyncAttempt<XElement> SerializeCore(UmbCheckoutConfiguration item, SyncSerializerOptions options)
+        {
+            var node = new XElement(ItemType,
+                new XAttribute("Id", item.Id));
+
+            node.Add(new XElement("BasketInCookieExpiry", item.BasketInCookieExpiry));
+            node.Add(new XElement("BasketInDatabaseExpiry", item.BasketInDatabaseExpiry));
+            var cancelPageUrl = new XElement("CancelPageUrl");
+            var cancelPage = item.CancelPageUrl.FirstOrDefault();
+            if (cancelPage != null)
+            {
+                cancelPageUrl.Add(new XElement("Icon", cancelPage.Icon));
+                cancelPageUrl.Add(new XElement("Name", cancelPage.Name));
+                cancelPageUrl.Add(new XElement("Published", cancelPage.Published));
+                cancelPageUrl.Add(new XElement("Trashed", cancelPage.Trashed));
+                cancelPageUrl.Add(new XElement("Udi", cancelPage.Udi));
+                cancelPageUrl.Add(new XElement("Url", cancelPage.Url));
+            }
+            node.Add(cancelPageUrl);
+            var successPageUrl = new XElement("SuccessPageUrl");
+            var successPage = item.SuccessPageUrl.FirstOrDefault();
+            if (successPage != null)
+            {
+                successPageUrl.Add(new XElement("Icon", successPage.Icon));
+                successPageUrl.Add(new XElement("Name", successPage.Name));
+                successPageUrl.Add(new XElement("Published", successPage.Published));
+                successPageUrl.Add(new XElement("Trashed", successPage.Trashed));
+                successPageUrl.Add(new XElement("Udi", successPage.Udi));
+                successPageUrl.Add(new XElement("Url", successPage.Url));
+            }
+            node.Add(successPageUrl);
+            node.Add(new XElement("EnableShipping", item.EnableShipping));
+            node.Add(new XElement("StoreBasketInCookie", item.StoreBasketInCookie));
+            node.Add(new XElement("StoreBasketInDatabase", item.StoreBasketInDatabase));
+
+            return SyncAttempt<XElement>.Succeed(Consts.Configuration.ItemType, node, typeof(UmbCheckoutConfiguration), ChangeType.Export);
+        }
+
+        protected override SyncAttempt<UmbCheckoutConfiguration> DeserializeCore(XElement node, SyncSerializerOptions options)
+        {
+            var configuration = node.Element(Consts.Configuration.ItemType);
+            var item = new UmbCheckoutConfiguration
+            {
+                StoreBasketInDatabase = configuration!.Element("StoreBasketInDatabase").ValueOrDefault(false)
+            };
+
+
+            return SyncAttempt<UmbCheckoutConfiguration>.Succeed("Configuration", item, ChangeType.Import, Array.Empty<uSyncChange>());
+        }
+
+        public override UmbCheckoutConfiguration FindItem(int id) => _configurationService.GetConfiguration().Result ?? new UmbCheckoutConfiguration();
+
+        public override UmbCheckoutConfiguration FindItem(Guid key) => _configurationService.GetConfiguration().Result ?? new UmbCheckoutConfiguration();
+
+        public override UmbCheckoutConfiguration FindItem(string alias) => null!;
+
+        public override void SaveItem(UmbCheckoutConfiguration item) => _configurationService.UpdateConfiguration(item);
+
+        public override void DeleteItem(UmbCheckoutConfiguration item)
+        {
+        }
+
+        public override string ItemAlias(UmbCheckoutConfiguration item) => string.Empty;
+
+        public override Guid ItemKey(UmbCheckoutConfiguration item) => Guid.Empty;
+    }
+}
